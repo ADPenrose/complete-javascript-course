7b58bf43580a93ad8ab8ee22bc52abe94b810355
Arturo Avila <mysticmetromx@gmail.com>
2023-07-26 10:50:05 -0600

    Added code for 3rd challenge

--- 01-Fundamentals-Part-1/challenges/script.js
+++ 01-Fundamentals-Part-1/challenges/script.js
@@ -1,19 +1,33 @@
-// 1.
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
+// // 1.
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
 
-let BMIMark = massMark / (heightMark * heightMark);
-let BMIJohn = massJohn / (heightJohn * heightJohn);
+// let BMIMark = massMark / (heightMark * heightMark);
+// let BMIJohn = massJohn / (heightJohn * heightJohn);
 
-const markHigherBMI = BMIMark > BMIJohn;
+// const markHigherBMI = BMIMark > BMIJohn;
 
-console.log(BMIMark, BMIJohn, markHigherBMI);
+// console.log(BMIMark, BMIJohn, markHigherBMI);
 
-// 2.
-if (BMIMark > BMIJohn) {
-	console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+// // 2.
+// if (BMIMark > BMIJohn) {
+// 	console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+// } else {
+// 	console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+// }
+
+// 3.
+const scoreDolphins = (97 + 112 + 101) / 3;
+const scoreKoalas = (109 + 95 + 106) / 3;
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+	console.log('Dolphins win the trophy');
+} else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
+	console.log('Koalas win the trophy');
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100) {
+	console.log('Both win the trophy');
 } else {
-	console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+	console.log('No one wins the trophy :c');
 }
