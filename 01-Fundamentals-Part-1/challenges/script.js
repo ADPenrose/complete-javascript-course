87f4da94e2bd2ee91b919d27387d9f4b92e5ccd6
Arturo Avila <mysticmetromx@gmail.com>
2023-07-27 23:13:49 -0600

    Completed part 1 of the fundamentals

--- 01-Fundamentals-Part-1/challenges/script.js
+++ 01-Fundamentals-Part-1/challenges/script.js
@@ -19,15 +19,25 @@
 // }
 
 // 3.
-const scoreDolphins = (97 + 112 + 101) / 3;
-const scoreKoalas = (109 + 95 + 106) / 3;
+// const scoreDolphins = (97 + 112 + 101) / 3;
+// const scoreKoalas = (109 + 95 + 106) / 3;
 
-if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
-	console.log('Dolphins win the trophy');
-} else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
-	console.log('Koalas win the trophy');
-} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100) {
-	console.log('Both win the trophy');
-} else {
-	console.log('No one wins the trophy :c');
-}
+// if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+// 	console.log('Dolphins win the trophy');
+// } else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
+// 	console.log('Koalas win the trophy');
+// } else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100) {
+// 	console.log('Both win the trophy');
+// } else {
+// 	console.log('No one wins the trophy :c');
+// }
+
+// 4.
+const billValue = 275;
+const tip =
+	billValue >= 50 && billValue <= 300 ? billValue * 0.15 : billValue * 0.2;
+console.log(
+	`The bill was ${billValue}, the tip was ${tip}, and the total value is ${
+		billValue + tip
+	}`
+);
