5c0401dcd0012fea947b5ccefc2fc40b32ae82c1
Arturo Avila <mysticmetromx@gmail.com>
2023-08-01 10:27:08 -0600

    Finished second JS fundamentals session

--- 02-Fundamentals-Part-2/starter/challenges.js
+++ 02-Fundamentals-Part-2/starter/challenges.js
@@ -18,50 +18,52 @@
 // const scoreKoalas = calcAverage(65, 54, 49);
 // checkWinner(scoreDolphins, scoreKoalas);
 
-// 2.
-function calcTip(billValue) {
-	return billValue >= 50 && billValue <= 300
-		? billValue * 0.15
-		: billValue * 0.2;
-}
+// // 2.
+// function calcTip(billValue) {
+// 	return billValue >= 50 && billValue <= 300
+// 		? billValue * 0.15
+// 		: billValue * 0.2;
+// }
 
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
-// The same function, but implemented as an arrow function
-// const calcTip = (billValue) =>
-// 	billValue >= 50 && billValue <= 300 ? billValue * 0.1 : billValue * 0.2;
+// // The same function, but implemented as an arrow function
+// // const calcTip = (billValue) =>
+// // 	billValue >= 50 && billValue <= 300 ? billValue * 0.1 : billValue * 0.2;
 
-console.log(calcTip(40));
+// console.log(calcTip(40));
+
+// // 3.
+// const mark = {
+// 	fullName: 'Mark Miller',
+// 	mass: 78,
+// 	height: 1.69,
+// 	calcBMI: function () {
+// 		this.bmi = this.mass / (this.height * this.height);
+// 		return this.bmi;
+// 	},
+// };
+
+// const john = {
+// 	fullName: 'John Smith',
+// 	mass: 92,
+// 	height: 1.95,
+// 	calcBMI: function () {
+// 		this.bmi = this.mass / (this.height * this.height);
+// 		return this.bmi;
+// 	},
+// };
+
+// if (mark.calcBMI() > john.calcBMI()) {
+// 	console.log(
+// 		`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`
+// 	);
+// } else {
+// 	console.log(
+// 		`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`
+// 	);
+// }
 
 // 3.
-const mark = {
-	fullName: 'Mark Miller',
-	mass: 78,
-	height: 1.69,
-	calcBMI: function () {
-		this.bmi = this.mass / (this.height * this.height);
-		return this.bmi;
-	},
-};
-
-const john = {
-	fullName: 'John Smith',
-	mass: 92,
-	height: 1.95,
-	calcBMI: function () {
-		this.bmi = this.mass / (this.height * this.height);
-		return this.bmi;
-	},
-};
-
-if (mark.calcBMI() > john.calcBMI()) {
-	console.log(
-		`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`
-	);
-} else {
-	console.log(
-		`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`
-	);
-}
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -70,55 +70,143 @@ function fruitProcessor(apples, oranges) {
 }
 console.log(fruitProcessor(2, 3)); */
 
-// Arrays
-const friends = ['Luis', 'Santy', 'Omar'];
-console.log(friends[0]);
-console.log(friends.length);
-console.log(friends[friends.length - 1]);
+// // Arrays
+// const friends = ['Luis', 'Santy', 'Omar'];
+// console.log(friends[0]);
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
 
-// Exercise
-const calcAge = function (birthYear) {
-	return 2037 - birthYear;
-};
+// // Exercise
+// const calcAge = function (birthYear) {
+// 	return 2037 - birthYear;
+// };
 
-const years = [1991, 1984, 2008, 2020];
+// const years = [1991, 1984, 2008, 2020];
 
-const age1 = calcAge(years[0]);
-const age2 = calcAge(years[1]);
-const age3 = calcAge(years[2]);
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[2]);
 
-const ages = [age1, age2, age3];
-console.log(ages);
+// const ages = [age1, age2, age3];
+// console.log(ages);
 
-// Objects
-const jonas = {
-	firstName: 'Jonas',
-	lastName: 'Schmedtmann',
-	birthYear: 1991,
-	job: 'teacher',
-	friends: ['Michael', 'Peter', 'Steven'],
-	hasDriversLicense: true,
-	calcAge: function () {
-		this.age = 2037 - this.birthYear;
-		return this.age; // Returning the value is a good practice
-	},
-	getSummary: function () {
-		return `${this.firstName} is a ${this.calcAge()}-year old ${
-			this.job
-		}, and he ${
-			this.hasDriversLicense
-				? "has a driver's license"
-				: "doesn't have a driver's license"
-		}.`;
-	},
-};
+// // Objects
+// const jonas = {
+// 	firstName: 'Jonas',
+// 	lastName: 'Schmedtmann',
+// 	birthYear: 1991,
+// 	job: 'teacher',
+// 	friends: ['Michael', 'Peter', 'Steven'],
+// 	hasDriversLicense: true,
+// 	calcAge: function () {
+// 		this.age = 2037 - this.birthYear;
+// 		return this.age; // Returning the value is a good practice
+// 	},
+// 	getSummary: function () {
+// 		return `${this.firstName} is a ${this.calcAge()}-year old ${
+// 			this.job
+// 		}, and he ${
+// 			this.hasDriversLicense
+// 				? "has a driver's license"
+// 				: "doesn't have a driver's license"
+// 		}.`;
+// 	},
+// };
 
-console.log(jonas.getSummary());
+// console.log(jonas.getSummary());
 
-const interestedIn = prompt('What do you want to know?');
+// const interestedIn = prompt('What do you want to know?');
 
-if (jonas[interestedIn]) {
-	console.log(jonas[interestedIn]);
-} else {
-	console.log('Sorry, that info does no exist.');
+// if (jonas[interestedIn]) {
+// 	console.log(jonas[interestedIn]);
+// } else {
+// 	console.log('Sorry, that info does no exist.');
+// }
+
+// For loop
+// for (let rep = 1; rep <= 10; rep++) {
+// 	console.log(`Lifting weights repetition ${rep}`);
+// }
+
+// const jonasArray = [
+// 	'Jonas',
+// 	'Schmedtmann',
+// 	2037 - 1991,
+// 	'teacher',
+// 	['Michael', 'Peter', 'Steven'],
+// ];
+
+// const types = [];
+
+// for (let i = 0; i < jonasArray.length; i++) {
+// 	console.log(jonasArray[i]);
+// 	types.push(typeof jonasArray[i]);
+// }
+
+// console.log(types);
+
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
+
+// for (let i = 0; i < years.length; i++) {
+// 	ages.push(2037 - years[i]);
+// }
+
+// console.log(ages);
+
+// // Continue and Break statements
+// const jonasArray = [
+// 	'Jonas',
+// 	'Schmedtmann',
+// 	2037 - 1991,
+// 	'teacher',
+// 	['Michael', 'Peter', 'Steven'],
+// ];
+
+// // Continue Statement
+// for (let i = 0; i < jonasArray.length; i++) {
+// 	if (typeof jonasArray[i] !== 'string') continue;
+// 	else console.log(jonasArray[i]);
+// }
+
+// // Break Statement
+// for (let i = 0; i < jonasArray.length; i++) {
+// 	if (typeof jonasArray[i] !== 'string') break;
+// 	else console.log(jonasArray[i]);
+// }
+
+// // Looping backwards
+// const jonasArray = [
+// 	'Jonas',
+// 	'Schmedtmann',
+// 	2037 - 1991,
+// 	'teacher',
+// 	['Michael', 'Peter', 'Steven'],
+// ];
+
+// for (let i = jonasArray.length - 1; i >= 0; i--) {
+// 	console.log(jonasArray[i]);
+// }
+
+// // Loop inside a loop
+// for (let exercise = 1; exercise < 4; exercise++) {
+// 	console.log(`--------- Starting exercise ${exercise}`);
+// 	for (let rep = 1; rep < 6; rep++) {
+// 		console.log(`Lifting weight repetition ${rep}`);
+// 	}
+// }
+
+// While Loop with a defined number of iterations
+let rep = 1;
+while (rep <= 10) {
+	console.log(`Lifting weight repetition ${rep}`);
+	rep++;
+}
+
+// While loop with an undefined number of iterations
+let dice = Math.trunc(Math.random() * 6) + 1;
+while (dice !== 6) {
+	console.log(`You rolled a ${dice}`);
+	dice = Math.trunc(Math.random() * 6) + 1;
+	if (dice === 6) console.log('Loop is about to end.');
 }
