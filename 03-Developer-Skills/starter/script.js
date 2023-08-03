963ba18c7a0f346b67f8d5aad420ca0b3b138887
Arturo Avila <mysticmetromx@gmail.com>
2023-08-03 10:50:21 -0600

    Finished editor setup and challenge regarding debugging

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const printForecast = function (arr) {
+	let arrayFormatedTemps = [];
+	for (let i = 0; i < arr.length; i++) {
+		arrayFormatedTemps.push(`... ${arr[i]}°C in ${i + 1} days `);
+	}
+	console.log(arrayFormatedTemps.join(''));
+};
+
+printForecast([17, 21, 23]);
