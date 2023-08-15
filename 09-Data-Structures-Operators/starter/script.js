481c5b8677caf0b9ecc4d41fcdc6f8299f8b5684
Arturo Avila <mysticmetromx@gmail.com>
2023-08-14 22:04:32 -0600

    Finished destructuring arrays/objects sessions

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -2,28 +2,109 @@
 
 // Data needed for a later exercise
 const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+	'_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+	name: 'Classico Italiano',
+	location: 'Via Angelo Tavanti 23, Firenze, Italy',
+	categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+	starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+	mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+	order: function (starterIndex, mainIndex) {
+		return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+	},
+	// Destructuring the arguments of the function as they are passed
+	orderDelivery: function ({
+		starterIndex = 1,
+		mainIndex = 0,
+		time = '20:00',
+		address,
+	}) {
+		console.log(
+			`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delirever to ${address} at ${time}`
+		);
+	},
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+	openingHours: {
+		thu: {
+			open: 12,
+			close: 22,
+		},
+		fri: {
+			open: 11,
+			close: 23,
+		},
+		sat: {
+			open: 0, // Open 24 hours
+			close: 24,
+		},
+	},
 };
+
+restaurant.orderDelivery({
+	time: '22:30',
+	address: 'Via del Sole, 21',
+	mainIndex: 2,
+	starterIndex: 2,
+});
+restaurant.orderDelivery({
+	address: 'Via del Sole, 21',
+	starterIndex: 2,
+});
+
+// Destructuring objects
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+// Using custom names
+const {
+	name: retaurantName,
+	openingHours: hours,
+	categories: tags,
+} = restaurant;
+console.log(retaurantName, hours, tags);
+
+// Setting default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj); // The parenthesis is needed so that JS compiles this correctly.
+console.log(a, b);
+
+// Nested objects
+const {
+	fri: { open, close },
+} = openingHours;
+console.log(open, close);
+
+// // Array Destructuring
+// const arr = [2, 3, 4];
+
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+
+// let [first, , third] = restaurant.categories;
+// console.log(first, third);
+
+// // Swapping two variables
+// [first, third] = [third, first];
+// console.log(first, third);
+
+// // Destructuring the return of a function (which happens to be an array)
+// console.log(restaurant.order(2, 0));
+// const [starter, main] = restaurant.order(2, 0);
+// console.log(starter, main);
+
+// // Nested arrays. Getting 2, 5 and 6
+// const nested = [2, 4, [5, 6]];
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
+
+// // Setting default values for destructured values
+// const [p = 1, q = 1, r = 1] = [8, 9];
+// console.log(p, q, r);
