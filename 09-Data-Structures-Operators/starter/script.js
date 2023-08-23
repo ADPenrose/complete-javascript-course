5fd40ef51f8a5cdd0ba30551cd2aab9c0ff3a151
Arturo Avila <mysticmetromx@gmail.com>
2023-08-23 08:34:00 -0600

    Added code until coding challenge 1 for lesson 9

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -62,6 +62,129 @@ restaurant.orderDelivery({
 	starterIndex: 2,
 });
 
+// Coding Challenge #1
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+const game = {
+	team1: 'Bayern Munich',
+	team2: 'Borrussia Dortmund',
+	players: [
+		[
+			'Neuer',
+			'Pavard',
+			'Martinez',
+			'Alaba',
+			'Davies',
+			'Kimmich',
+			'Goretzka',
+			'Coman',
+			'Muller',
+			'Gnarby',
+			'Lewandowski',
+		],
+		[
+			'Burki',
+			'Schulz',
+			'Hummels',
+			'Akanji',
+			'Hakimi',
+			'Weigl',
+			'Witsel',
+			'Hazard',
+			'Brandt',
+			'Sancho',
+			'Gotze',
+		],
+	],
+	score: '4:0',
+	scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+	date: 'Nov 9th, 2037',
+	odds: {
+		team1: 1.33,
+		x: 3.25,
+		team2: 6.5,
+	},
+};
+
+// 1.
+const [players1, players2] = [...game.players];
+
+// 2.
+const [gk, ...fieldPlayers] = players1;
+
+// 3.
+const allPlayers = [...players1, ...players2];
+
+// 4.
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+
+// 5.
+const { team1, x: draw, team2 } = game.odds;
+
+// 6.
+function printGoals(...players) {
+	console.log('########## PLAYERS ##########');
+	for (let i = 0; i < players.length; i++) {
+		console.log(players[i]);
+	}
+	console.log('########## GOALS ##########');
+	console.log(players.length);
+}
+
+printGoals(...game.scored);
+
+// 7.
+game.odds.team1 > game.odds.team2 &&
+	console.log('Team 2 is more likely to win.');
+game.odds.team1 < game.odds.team2 &&
+	console.log('Team 1 is more likely to win.');
+// // Logical Assignment Operators
+// const rest1 = {
+// 	name: 'Capri',
+// 	numGuests: 0,
+// };
+
+// const rest2 = {
+// 	name: 'La Piazza',
+// 	owner: 'Giovanni Rossi',
+// };
+
+// // Establishing default values using OR short-circuiting
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// //Doing the same thing, but with the OR logical assignment operators
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// // Using ?? to account for the case where numGuests is 0
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
+
+// // Using the AND logical assignment operator
+// rest1.owner &&= '<ANONYMOUS>';
+// rest2.owner &&= '<ANONYMOUS>';
+
+// console.log(rest1);
+// console.log(rest2);
+
 // Short-circuiting with the AND operator
 // console.log(0 && 'Jonas');
 // console.log(7 && 'Jonas');
@@ -77,9 +200,9 @@ restaurant.orderDelivery({
 // console.log(restaurant);
 
 // The nullish coalescing operator
-restaurant.numGuestsCorrect = 0;
-restaurant.numGuestsCorrect = restaurant.numGuestsCorrect ?? 10;
-console.log(restaurant);
+// restaurant.numGuestsCorrect = 0;
+// restaurant.numGuestsCorrect = restaurant.numGuestsCorrect ?? 10;
+// console.log(restaurant);
 
 // // Returns 'Hello'
 // console.log(undefined || 0 || '' || 'Hello' || 23 || null);
