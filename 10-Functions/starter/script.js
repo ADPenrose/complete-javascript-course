6ce56a1267a1b3b3fa5d88a3e44782c30f09d02e
Arturo Avila <mysticmetromx@gmail.com>
2023-09-10 17:27:45 -0600

    Completed challenge 1 of section 10

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -24,32 +24,214 @@
 // createBooking('LH123', 2);
 // createBooking('LH123', undefined, 800);
 
-const flight = 'LH234';
-const arturo = {
-  name: 'Arturo Avila',
-  passport: 343434,
-};
+// const flight = 'LH234';
+// const arturo = {
+//   name: 'Arturo Avila',
+//   passport: 343434,
+// };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr. ' + passenger.name;
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
 
-  if (passenger.passport === 343434) {
-    alert('Checked in');
-  } else alert('Wrong passport');
-};
+//   if (passenger.passport === 343434) {
+//     alert('Checked in');
+//   } else alert('Wrong passport');
+// };
 
+// // checkIn(flight, arturo);
+// // console.log(flight);
+// // console.log(arturo);
+
+// // // The latter is the same as...
+// // const flightNum = flight;
+// // const passenger = arturo;
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 10000000);
+// };
+
+// newPassport(arturo);
 // checkIn(flight, arturo);
-// console.log(flight);
-// console.log(arturo);
 
-// // The latter is the same as...
-// const flightNum = flight;
-// const passenger = arturo;
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
 
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 10000000);
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
+
+// // Higher-order function
+// const transformer = function (str, fn) {
+//   console.log(`Original String: ${str}`);
+//   console.log(`Transformed string: ${fn(str)}`);
+//   // Using the name method of the function to obtain its name
+//   console.log(`Transformed by: ${fn.name}`);
+// };
+
+// transformer('JavaScript is the best!', upperFirstWord);
+// transformer('JavaScript is the best!', oneWord);
+
+// Returning functions
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+// const greeterHey = greet('Hey');
+// greeterHey('Arnold');
+// greeterHey('Steven');
+
+// greet('Hello')('Arturo');
+
+// // The same function as the one above, but using arrow funcions
+// const greetArrow = greeting => name => console.log(`${greeting} ${name}`);
+// greetArrow('Hello')('Lula');
+
+// Call and apply methods
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
+//     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+//   },
+// };
+
+// lufthansa.book(239, 'Jonas Schmedtmann');
+// lufthansa.book(635, 'John Smith');
+// console.log(lufthansa);
+
+// const eurowings = {
+//   airline: 'Eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
+
+// const book = lufthansa.book;
+// // Storing a method inside a new variable, so that we can use it as an external function. This won't work.
+// // book(23, 'Sarah Williams');
+
+// // Using the call function method
+// book.call(eurowings, 23, 'Sarah Williams');
+
+// // Using the apply function method
+// const flightData = [583, 'George Copper'];
+// book.apply(eurowings, flightData);
+
+// // Using the bind function method
+// const bookEW = book.bind(eurowings);
+// bookEW(23, 'Steven Williams');
+
+// const bookEW23 = book.bind(eurowings, 23);
+// bookEW23('Stephen Strange');
+
+// // Bind with event listeners
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//   console.log(this);
+//   this.planes++;
+//   console.log(this.planes);
+// };
+
+// const button = document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// // Partial application
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+// // We define the this object as null, since in this case it is not used.
+// const addVAT = addTax.bind(null, 0.23);
+// console.log(addVAT(23));
+
+// // Using high-order returning functions instead of the bind method as a challenge
+// const addVATChallenge = function (rate) {
+//   return function (value) {
+//     console.log(value + value * rate);
+//   };
+// };
+
+// const addVAT2 = addVATChallenge(0.23);
+// console.log(addVAT2(23));
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const optionSelected = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+    if (
+      typeof optionSelected !== 'number' ||
+      optionSelected < 0 ||
+      optionSelected > 3
+    ) {
+      alert('Invalid Option! Try again.');
+    } else {
+      this.answers[optionSelected]++;
+      this.displayResults();
+    }
+  },
+  displayResults(type = 'array') {
+    if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}.`);
+    } else if (type === 'array') {
+      console.log(this.answers);
+    }
+  },
 };
 
-newPassport(arturo);
-checkIn(flight, arturo);
+// Calling the poll.registerNewAnswer method whenever the user clicks on a button
+const answerButton = document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// Bonus
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [5, 2, 3] }, 'array');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'array');
