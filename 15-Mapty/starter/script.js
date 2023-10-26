ec364ff864f03ef5ec27d4efaf1544e4028960b3
Arturo Avila <mysticmetromx@gmail.com>
2023-10-25 23:14:17 -0600

    Added until Geolocation API

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,17 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// Using the Geolocation API
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    // Getting the coordinates
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    // Creating a Google Maps URL using our coords data.
+    console.log(`https://www.google.com.mx/maps/@${latitude},${longitude}`);
+  },
+  function () {
+    alert('Could not get your position!');
+  }
+);
