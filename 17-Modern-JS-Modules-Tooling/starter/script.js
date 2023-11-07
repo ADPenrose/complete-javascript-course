8979375da22decee4d81702a60da3e4a410a4e80
Arturo Avila <mysticmetromx@gmail.com>
2023-11-06 23:01:26 -0600

    Completed until CommonJS modules

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,78 @@
+// Importing module
+// We can change the names of the imported data.
+// import { addToCart, totalPrice as price, qt } from './shoppingCart.js';
+// Calling the imported function and the imported values.
+// addToCart('bread', 5);
+// console.log(price, qt);
+
+console.log('Importing module');
+
+// Importing all of the exports of a module at once.
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+
+// Importing the default export of a module.
+import add from './shoppingCart.js';
+add('pizza', 2);
+
+// Mixing default and named imports. USUALLY NOT DONE IN PRACTICE.
+// import add, { totalPrice as price, qt } from './shoppingCart.js';
+
+// This will not work, since the value is not exported in the module.
+// console.log(shippingCost);
+
+// Top-level await
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  console.log(data);
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+// Using the then method to consume the promise. Not very clean.
+// const lastPost = getLastPost();
+// lastPost.then(response => console.log(response));
+
+// We can use top-level await for this
+// const lastPostAwaited = await getLastPost();
+// console.log(lastPostAwaited);
+
+// // The old module pattern. We have a closure in here.
+// const ShoppingCart2 = (function () {
+//   const cart = [];
+//   const shippingCost = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
+
+//   const addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} added to cart`);
+//   };
+
+//   const orderStock = function (product, quantity) {
+//     console.log(`${quantity} ${product} ordered from supplier`);
+//   };
+
+//   // All of the stuff declared above is not public. To make a public
+//   // API out of it, we need to return the data that we want to have on
+//   // that API inside of an object.
+//   return { addToCart, cart, totalPrice, totalQuantity };
+// })();
+// // Accesible
+// ShoppingCart2.addToCart('apple', 4);
+// // Not accesible (undefined)
+// console.log(ShoppingCart2.shippingCost);
+
+// CommonJS Modules (will not work on the web, but will work in NodeJs)
+// Export (imagine that this is on a ./shoppingCart.js file)
+// exports.addToCart = function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity} ${product} added to cart`);
+// };
+
+// // Import
+// const { addToCart } = requiere('/shoppingCart.js');
---
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -0,0 +1,29 @@
+// Exporting module
+console.log('Exporting module');
+
+// Blocking code
+// console.log('Start fetching users');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finished fetching');
+
+// We can only use these values here, in this file.
+const shippingCost = 10;
+const cart = [];
+
+// Named export.
+// export const addToCart = function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity} ${product} added to cart`);
+// };
+
+// Exporting multiple things at the same time using named exports.
+const totalPrice = 237;
+const totalQuantity = 23;
+// We can change the names of the exported data.
+export { totalPrice, totalQuantity as qt };
+
+// Default export. Used when we only want to export one thing per module.
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}
